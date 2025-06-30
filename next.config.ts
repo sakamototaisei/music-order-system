46db9c24b81ebcb73c87dd6e87e6283953c316ae
ifreek-mobile <ifreekmobile123@gmail.com>
2025-06-30 23:08:29 +0900

    fix: Allow 'unsafe-eval' in CSP to resolve deployment error

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src *; font-src 'self'; connect-src *;",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
+
